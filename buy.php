46a6574a3e7003728ebe0f18d98838c3573c67b8
pathum2k <blackdragon20002k@gmail.com>
2021-10-08 17:17:12 +0530

    up

---
+++ buy.php
@@ -0,0 +1,38 @@
+<?php
+
+
+
+?>
+
+<!DOCKTYPE html>
+
+<html>
+<head>
+    <title>Buy</title>
+    <link rel="stylesheet" href="./css/buy.css">
+
+
+</head>
+<body>
+    <div class="lolcatpage">
+        <div class="locathead">
+
+                <h3>location</h3>
+        </div>
+        
+                <div class="addresstype">
+
+                    <div class="textaddress">
+                        <label class="buytxt2">text address</label>  
+                        <textarea id="address"></textarea>
+                    </div>
+
+                    <div class="map">
+                        <label class="buytext2">Location Add on map</label>
+                    </div>
+
+                </dic>    
+    </div>
+
+</body>
+</html>
---
+++ payment.php
@@ -0,0 +1,64 @@
+<?php
+
+
+?>
+
+<!DOCKTYPE html>
+<html>
+<head>
+
+    <title>Online Grocery</title>
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/pay.css">
+
+</head>
+<body>
+    <div class="main">
+
+        <div class="headbox">
+            <p class="headtext">easy shopping quality products</p>
+        </div>
+
+        <div class="headbase">
+
+            <div class="logodiv">
+                    <img class="logo" src="images/logoo.jpg" alt="logo"/>
+            </div>   
+
+            <div class="shopname">
+                    <p>Groserie</p>
+            </div>
+
+        </div>
+
+        <div class="menu">
+        
+            <a href="payment.php" class="button"> home     </a>
+            <a href="" class="button"> category </a>
+            <a href="" class="button"> about us </a>
+            <a href="" class="button"> login    </a>
+
+        </div>
+
+    </div>
+
+<div class="paydiv">
+<label class="paytext">price</label>
+
+</br></br>
+<label class="paytext">discount</label>
+
+</br></br>
+<label class="paytext">location</label>
+
+</br></br>
+<label class="paytext">Payment option</label>
+
+
+
+
+</div>
+
+    <hr>    
+</body>
+</html>    
