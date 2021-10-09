30cdbdcb3641fb2823c1757b36e6ca4979bd6284
pathum2k <blackdragon20002k@gmail.com>
2021-10-09 12:01:39 +0530

    i chanege

---
+++ category/category.php
@@ -0,0 +1,101 @@
+<?php
+
+
+?>
+
+<!DOCKTYPE html>
+<html>
+<head>
+
+    <title>Online Grocery</title>
+    <link rel="stylesheet" href="../css/index.css">
+
+</head>
+<body>
+    <div class="main">
+
+                <div class="headbox">
+                    <p class="headtext">easy shopping quality products</p>
+                </div>
+
+        <div class="headbase">
+
+                <div class="logodiv">
+                        <img class="logo" src="images/logoo.jpg" alt="logo"/>
+                </div> 
+
+                <div class="shopname" >
+                        <lable>Groserie</lable>
+                </div>
+
+        
+        </div>
+            <div class="menu">
+            
+                <a href="payment.php" class="button"> Home     </a>
+                <a href="buy.php" class="button"> Category </a>
+                <a href="" class="button"> About us </a>
+                <a href="" class="button"> Login    </a>
+
+            </div>
+    </div>
+    
+
+
+    <div class="catdiv">
+
+        <div class="catbox">
+
+            <div class="imgdiv">
+                <img class="catimg" src="./images/vegitables.jpg" alt="Vegitable"/>
+            </div>
+
+                <a href="" class="catname"> Vegitables </a>
+            </div>
+
+
+            <div class="catbox">
+
+                <div class="imgdiv">
+                    <img class="catimg" src="./images/fruit.jpg" alt="Fruit"/>
+                </div>
+
+                    <a href="" class="catname"> Fruits </a>
+            </div>
+
+
+            <div class="catbox">
+
+                <div class="imgdiv">
+                    <img class="catimg" src="./images/drinks.jpg" alt="Juice Item"/>
+                </div>
+
+                    <a href="" class="catname"> Juice Item </a>
+            </div>
+
+
+            <div class="catbox">
+
+                <div class="imgdiv">
+                    <img class="catimg" src="./images/snacks.jpg" alt="Snack Item"/>
+                </div>
+
+                    <a href="" class="catname"> Snacks Item </a>
+            </div>
+
+
+            <div class="catbox">
+
+                <div class="imgdiv">
+                    <img class="catimg" src="./images/grocery.jpg" alt="Grocery Item"/>
+                </div>
+
+                    <a href="" class="catname"> Grocery Item </a>
+            </div>
+
+
+
+    </div>
+<hr>    
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,16 +1,11 @@
-<?php
-
-
-?>
-
-<!DOCKTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 
-    <title>Online Grocery</title>
-    <link rel="stylesheet" href="./css/index.css">
+        <link rel="stylesheet" href="x.css">
 
 </head>
+
 <body>
     <div class="main">
 
@@ -21,7 +16,7 @@
         <div class="headbase">
 
                 <div class="logodiv">
-                        <img class="logo" src="images/logoo.jpg" alt="logo"/>
+                        <img class="logo" src="../images/logoo.jpg" alt="logo"/>
                 </div> 
 
                 <div class="shopname" >
@@ -32,70 +27,12 @@
         </div>
             <div class="menu">
             
-                <a href="payment.php" class="button"> Home     </a>
-                <a href="buy.php" class="button"> Category </a>
+                <a href="" class="button"> Home     </a>
+                <a href="./category/category.php" class="button"> Category </a>
                 <a href="" class="button"> About us </a>
                 <a href="" class="button"> Login    </a>
 
             </div>
     </div>
-    
-
-
-    <div class="catdiv">
-
-        <div class="catbox">
-
-            <div class="imgdiv">
-                <img class="catimg" src="./images/vegitables.jpg" alt="Vegitable"/>
-            </div>
-
-                <a href="" class="catname"> Vegitables </a>
-            </div>
-
-
-            <div class="catbox">
-
-                <div class="imgdiv">
-                    <img class="catimg" src="./images/fruit.jpg" alt="Fruit"/>
-                </div>
-
-                    <a href="" class="catname"> Fruits </a>
-            </div>
-
-
-            <div class="catbox">
-
-                <div class="imgdiv">
-                    <img class="catimg" src="./images/drinks.jpg" alt="Juice Item"/>
-                </div>
-
-                    <a href="" class="catname"> Juice Item </a>
-            </div>
-
-
-            <div class="catbox">
-
-                <div class="imgdiv">
-                    <img class="catimg" src="./images/snacks.jpg" alt="Snack Item"/>
-                </div>
-
-                    <a href="" class="catname"> Snacks Item </a>
-            </div>
-
-
-            <div class="catbox">
-
-                <div class="imgdiv">
-                    <img class="catimg" src="./images/grocery.jpg" alt="Grocery Item"/>
-                </div>
-
-                    <a href="" class="catname"> Grocery Item </a>
-            </div>
-
-
-
-    </div>
-<hr>    
 </body>
 </html>
